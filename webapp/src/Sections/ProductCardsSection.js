5ea21d851c8e60234ddd517428ed7474f119167a
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-10-26 23:53:12 +0200

    cards

--- webapp/src/Sections/ProductCardsSection.js
+++ webapp/src/Sections/ProductCardsSection.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import TempImg from '../Assets/Images/alpine1.jpg'
 
 const ProductCardsSection = () => {
   return (
@@ -11,13 +13,17 @@ const ProductCardsSection = () => {
                         {/* 1 */}
                         <div className="card">
                             <div className="img"></div> 
+                            <img src={TempImg} alt="" />
                             <ul className="card-menu">
                                 <li><button type="button" className="btn btn-light btn-circle btn-sm"><a href = "#"><i className="fa-regular fa-heart"></i></a></button></li> 
                                 <li><button type="button" className="btn btn-light btn-circle btn-sm"><a href = "#"><i className="fa-solid fa-code-compare fa-flip-horizontal"></i></a></button></li> 
                                 <li><button type="button" className="btn btn-light btn-circle btn-sm"><a href = "#"><i className="fa-solid fa-bag-shopping"></i></a></button></li> 
                             </ul>
                             <div className="card-general">
-                                <button type= "button" className="btn special_button">QUICK VIEW</button>
+                                <NavLink to="/products/"id>
+                                    <button type= "button" className="btn special_button">
+                                      QUICK VIEW</button>
+                                </NavLink> 
                                 <div className="card-info">
                                     <p className="category">Category</p>
                                     <h1><strong>Modern Black Blouse</strong></h1>
--- webapp/src/Views/HomeView.js
+++ webapp/src/Views/HomeView.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import FooterSection from '../Sections/FooterSection'
 import TopMenuSection from '../Sections/TopMenuSection'
+import ProductCardsSection from '../Sections/ProductCardsSection'
 
 const HomeView = () => {
   return (
 
     <>
     <TopMenuSection />
+    <ProductCardsSection />
     <FooterSection />
     </>  
   
