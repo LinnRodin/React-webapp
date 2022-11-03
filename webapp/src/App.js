2a64e711cf03309ccf3a773b28a0172fe1bf986a
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-11-03 23:35:45 +0100

    updated

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import HomeView from './Views/HomeView';
 import CategoriesView from './Views/CategoriesView';
 import ProductsView from './Views/ProductsView';
@@ -16,13 +15,18 @@ import {ProductContext} from './Contexts/contexts';
 
 function App() {
 
-  const [products, setProducts] = useState([
-    {id: 1, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "https://images.pexels.com/photos/2433353/pexels-photo-2433353.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {id: 2, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "https://images.pexels.com/photos/257961/pexels-photo-257961.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {id: 3, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "https://images.pexels.com/photos/7165175/pexels-photo-7165175.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {id: 4, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5, img: "https://images.pexels.com/photos/4969872/pexels-photo-4969872.jpeg?auto=compress&cs=tinysrgb&w=600"},
-])
-  
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+        let result = await fetch('https://win22-webapi.azurewebsites.net/api/products')
+        setProducts(result.json());
+    }
+    fetchData();
+    
+}, []);
+
+
   return (
     <BrowserRouter>
       <ProductContext.Provider value={products}>
