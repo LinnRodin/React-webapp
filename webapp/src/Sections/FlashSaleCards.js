089480ae3f721f8c560d812e7f39080a23831a78
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-11-07 16:53:30 +0100

    updated

--- webapp/src/Sections/FlashSaleCards.js
+++ webapp/src/Sections/FlashSaleCards.js
@@ -6,21 +6,22 @@ const FlashSaleCards = ({ products = []}) => {
   return (
         <section className="flash-sale-left">
                 <div className="container">              
-                        <div className="img_left_box">
-                            <h1>2 FOR USD $29</h1>
-                        
-                        <div className="img_left_button">
-                            <a href = "#"><button type= "button" className="btn special_button">FLASH SALE</button></a>
-                        </div>
+                    <div className="img_left_box">
+                        <h1>2 FOR USD $29</h1>
+                    
+                    <div className="img_left_button">
+                        <a href = "#"><button type= "button" className="btn special_button">FLASH SALE</button></a>
+                    </div>
                     </div>
                 </div>  
                 <div className="products">
                 {
                     products.map( product => <ProductCard key={product.articleNumber} product={product} />)
                 } 
-            </div>
+                </div>
         </section>      
     )
 }
 
 export default FlashSaleCards
+
--- webapp/src/Sections/ShowCaseSection.js
+++ webapp/src/Sections/ShowCaseSection.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import showcase1 from '../Assets/Images/showcase-img-1.png';
+import showcase2 from '../Assets/Images/showcase-img-2.png';
 
 const ShowCaseSection = () => {
   return (
     <section className="showcase">
             <div className="container">
                 <div className="showcase-content">  
-                    <div><img src={'https://win22imgstorage.blob.core.windows.net/images/showcase-img-1.png'} className="_img-left" alt="female"/></div>
-                        <div><h1><strong>Sale Up To 50% off</strong></h1>
+                    <div><img src={showcase1} className="_img-left" alt="female"/></div>
+                        <div><h1><strong>Sale Up <br/> To 50% off</strong></h1>
                             <p>Online shopping free home delivery over $100</p>
                             <div className="general_button">
                             <a href = "#"><button type= "button" className="btn special_button">SHOP NOW</button></a>
                             </div>
                         </div>
-                    <div><img src={'https://win22imgstorage.blob.core.windows.net/images/showcase-img-2.png'} className="_img-right" alt="male"/></div>
+                    <div><img src={showcase2} className="_img-right" alt="male"/></div>
                 </div>
             </div>        
         </section>
