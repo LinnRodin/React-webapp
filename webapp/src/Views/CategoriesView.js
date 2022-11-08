78712dcd222f15dabf93e64dc823150be34f9d35
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-11-08 22:59:56 +0100

    404

--- webapp/src/Views/CategoriesView.js
+++ webapp/src/Views/CategoriesView.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import FooterSection from '../Sections/FooterSection'
 import TopMenuSection from '../Sections/TopMenuSection'
+import NotFoundImage from '../Assets/Images/404-Website-Page-Template.jpg'
+
+
 
 const CategoriesView = () => {
   window.top.document.title = 'Categories | Fixxo.'
   return (
     <>
     <TopMenuSection />
-    <FooterSection />
+      <div className="container">
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <img src={NotFoundImage} alt="404 Page Not Found" style={{ height: "768px" }} />
+          </div>
+        </div>
+      <FooterSection />
     </>  
   )
 }
--- webapp/src/Views/NotFoundView.js
+++ webapp/src/Views/NotFoundView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FooterSection from '../Sections/FooterSection'
 import TopMenuSection from '../Sections/TopMenuSection'
+import NotFoundImage from '../Assets/Images/404-Website-Page-Template.jpg'
 
 const NotFoundView = () => {
   window.top.document.title = 'NotFound | Fixxo.'
@@ -9,7 +10,12 @@ const NotFoundView = () => {
     
     <>
     <TopMenuSection />
-    <h1>404 - Page Not Found</h1>
+      <div className="container">
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <img src={NotFoundImage} alt="404 Page Not Found" style={{ height: "768px" }} />
+          <h1 className="ms-4" style={{ fontSize: "3rem" }}>The page you were looking for couldn't be found.</h1>
+        </div>
+      </div>
     <FooterSection />
     </>  
     
