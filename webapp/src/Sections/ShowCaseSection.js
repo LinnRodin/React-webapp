f626811b64dfa92902dd4286722dd27f65d67ddb
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-11-08 23:26:52 +0100

    updated

--- webapp/src/Sections/ShowCaseSection.js
+++ webapp/src/Sections/ShowCaseSection.js
@@ -4,17 +4,17 @@ import showcase2 from '../Assets/Images/showcase-img-2.png';
 
 const ShowCaseSection = () => {
   return (
-    <section className="showcase">
+        <section className="showcase">
             <div className="container">
                 <div className="showcase-content">  
-                    <div><img src={showcase1} className="_img-left" alt="female"/></div>
+                    <img src={showcase1} className="_img-left" alt="female"/>
                         <div><h1><strong>Sale Up <br/> To 50% off</strong></h1>
                             <p>Online shopping free home delivery over $100</p>
                             <div className="general_button">
                             <a href = "#"><button type= "button" className="btn special_button">SHOP NOW</button></a>
                             </div>
                         </div>
-                    <div><img src={showcase2} className="_img-right" alt="male"/></div>
+                    <img src={showcase2} className="_img-right" alt="male"/>
                 </div>
             </div>        
         </section>
