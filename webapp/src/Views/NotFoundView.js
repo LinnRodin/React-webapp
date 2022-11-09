b25072fef19d0e377d69767fe78041269d4a1c04
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-11-09 12:21:36 +0100

    updated

--- webapp/src/Views/NotFoundView.js
+++ webapp/src/Views/NotFoundView.js
@@ -13,7 +13,7 @@ const NotFoundView = () => {
       <div className="container">
         <div className="d-flex justify-content-center align-items-center vh-100">
           <img src={NotFoundImage} alt="404 Page Not Found" style={{ height: "768px" }} />
-          <h1 className="ms-4" style={{ fontSize: "3rem" }}>The page you were looking for couldn't be found.</h1>
+          <h1 className="ms-4" style={{ fontSize: "3rem" }}></h1>
         </div>
       </div>
     <FooterSection />
